76d20eb60913a1192b7de210e23466d7d21090db
rennieb1 <49140451+rennieb1@users.noreply.github.com>
2019-05-27 11:29:14 +0800

    Editing for pause menu
    Editing for pause menu (NOT COMPLETE)

---
+++ Assets/Prefabs/PlayerPause.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPause : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject gamePauseMenu;
+
+
+
+    private void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+
+    {
+        gamePauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        gamePauseMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+        gameIsPaused = true;
+    }
+
+}
