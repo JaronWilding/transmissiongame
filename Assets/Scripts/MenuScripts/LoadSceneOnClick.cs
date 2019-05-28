63abf8d3bf05b317f4cbd1750794524b00161be7
rennieb1 <rennieb1@iinet.net.au>
2019-05-28 09:17:22 +0800

    Pause screen added
    Pause screen added to the current test screen, not making it a prefab yet as i do not know what else will be happening with the game controller.

--- Assets/Scripts/MenuScripts/LoadSceneOnClick.cs
+++ Assets/Scripts/MenuScripts/LoadSceneOnClick.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadSceneOnClickMainMenuOnly : MonoBehaviour
+public class LoadSceneOnClick : MonoBehaviour
 {
 
 
