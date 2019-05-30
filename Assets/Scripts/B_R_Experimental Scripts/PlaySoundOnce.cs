a0acd6609359c8cc09190506fa831e73a28537e8
rennieb1 <rennieb1@iinet.net.au>
2019-05-30 21:57:12 +0800

    Have working audiolog ft J man
    Audio log plays once,  then deletes self, on press the item deactivates the box colilder to do this,

---
+++ Assets/Scripts/B_R_Experimental Scripts/PlaySoundOnce.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnce : MonoBehaviour
+{
+    public AudioClip audioLog;
+    AudioSource audioSource;
+    private bool hasPlayed = false;
+    private BoxCollider boxCol;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        boxCol = GetComponent<BoxCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "GameController" && Input.GetKeyDown(KeyCode.F))
+        {
+
+            audioSource.PlayOneShot(audioLog);
+            boxCol.enabled =false;
+            //   hasPlayed = true;
+            Destroy(gameObject, audioSource.clip.length);
+
+        }
+    }
+}
